48c4965f2c540927011fbdc8d1e6d54499dffb04
sanberkzulfikar <tosanberk@icloud.com>
2023-07-28 02:26:47 +0300

    go7

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
+const cors = require("cors");
 
 // express app
 const app = express();
