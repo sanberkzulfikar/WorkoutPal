bb6b048c3edfbdd22ff96a6ccc0f5f2c345705a0
sanberkzulfikar <tosanberk@icloud.com>
2023-07-28 02:11:05 +0300

    up6

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,31 +1,44 @@
-import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 // date fns
-import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
 const WorkoutDetails = ({ workout }) => {
-  const { dispatch } = useWorkoutsContext()
+  const { dispatch } = useWorkoutsContext();
 
   const handleClick = async () => {
-    const response = await fetch('/api/workouts/' + workout._id, {
-      method: 'DELETE'
-    })
-    const json = await response.json()
+    const response = await fetch(
+      "https://workoutpal-vnq3.onrender.com/api/workouts/" + workout._id,
+      {
+        method: "DELETE",
+      }
+    );
+    const json = await response.json();
 
     if (response.ok) {
-      dispatch({type: 'DELETE_WORKOUT', payload: json})
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
     }
-  }
+  };
 
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
-      <p><strong>Load (kg): </strong>{workout.load}</p>
-      <p><strong>Number of reps: </strong>{workout.reps}</p>
-      <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
-      <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+      <p>
+        <strong>Load (kg): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Number of reps: </strong>
+        {workout.reps}
+      </p>
+      <p>
+        {formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}
+      </p>
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        delete
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default WorkoutDetails
+export default WorkoutDetails;
