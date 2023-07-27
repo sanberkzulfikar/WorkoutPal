ccf1e17e02ed0b90b70b2df80b20f2ad6af444b8
sanberkzulfikar <tosanberk@icloud.com>
2023-07-28 02:19:59 +0300

    up6

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:4000",
+      "/api": "https://workoutpal-vnq3.onrender.com/api/workouts/",
     },
   },
   plugins: [react()],
